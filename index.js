c4a9b11158890fc4dfa40c0446348bd463478d72
JINOOK JUNG <jinook929@gmail.com>
2020-11-07 13:54:38 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function breakOut(array, changeValue, stopValue) {
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] === stopValue) {
+      break;
+    }
+    array[i] = changeValue;
+  }
+  
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue) {
+    for(let i = 0; i < array.length; i++) {
+    if(array[i] === skipValue) {
+      continue;
+    }
+    array[i] = changeValue;
+  }
+  
+  return array;
+}
+
+function findBy(array, findFn) {
+  var found = array.forEach(findFn);
+  return found;
+}
+
+function findFn(array, target) {
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] === target) {
+      return array[i];
+    }
+  }
+  return null;
+}
